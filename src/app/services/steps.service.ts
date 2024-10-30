56573a1c6dbfd958191a292a674e552038e92bde
rawl0408 <raulin4bcn@gmail.com>
2024-10-30 13:56:08 +0100

    Sprint 5 Ejercicio 3 - 50% creado

--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -9,20 +9,20 @@ export class StepsService {
     {
       title: 'Dedica moltes hores',
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-      img: '',
-      bgcolor: ''
+      img: 'assets/img/time_managment.svg',
+      bgcolor: '#008080'
     },
     {
       title: 'Programa projectes propis',
       description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-      img: '',
-      bgcolor: ''
+      img: 'assets/img/programming.svg',
+      bgcolor: '#C0C0C0'
     },
     {
       title: 'Procura descansar',
       description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      img: '',
-      bgcolor: ''
+      img: 'assets/img/meditation.svg',
+      bgcolor: '#FFD700'
     }
   ];
 
